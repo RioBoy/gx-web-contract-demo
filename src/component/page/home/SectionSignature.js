a1185c8c3489c22d2b835dd5b24b1f20a4c078eb
RioBoy <boyrio68@gmail.com>
2024-03-08 18:56:48 +0800

    add feature signature

--- src/component/page/home/SectionSignature.js
+++ src/component/page/home/SectionSignature.js
@@ -1,68 +1,89 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import SignaturePad from 'signature_pad';
 import { PageSubTitle } from '@/component/general/PageTitle';
 
 const SectionSignature = () => {
+  const [formRequest, setFormRequest] = useState({
+    file: '',
+  });
+  const [isEmptyCanvas, setIsEmptyCanvas] = useState(true);
+
+  const resizeCanvas = () => {
+    const canvas = document.getElementById('signature-canvas');
+
+    const ratio = Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    canvas.getContext('2d').scale(ratio, ratio);
+  };
+
+  const _handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit');
+  };
+
+  useEffect(() => {
+    const canvas = document.querySelector('canvas');
+    const signaturePad = new SignaturePad(canvas);
+
+    signaturePad.addEventListener(
+      'beginStroke',
+      () => {
+        setIsEmptyCanvas(false);
+      },
+      { once: true },
+    );
+
+    resizeCanvas();
+  }, []);
+
   return (
     <section className="container pt-1">
-      <div className="row align-items-center">
-        <div className="col-md-6">
-          <PageSubTitle title="Signature" />
+      <form onSubmit={_handleSubmit}>
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <PageSubTitle title="Signature" />
+          </div>
+
+          <div className="col-md-6 d-flex justify-content-end">
+            <button className="btn btn-transparent fs-16 text-blue-300 text-decoration-underline">
+              Upload Signature
+            </button>
+          </div>
         </div>
 
-        <div className="col-md-6 d-flex justify-content-end">
-          <button className="btn btn-transparent fs-16 text-blue-300 text-decoration-underline">
-            Upload Signature
-          </button>
-        </div>
-      </div>
+        <div className="row">
+          <div className="col-md-12 mb-4">
+            <div className="wp-signature">
+              <canvas
+                id="signature-canvas"
+                className="signature-canvas"
+              ></canvas>
 
-      <div className="row mb-4">
-        <div className="col-md-12">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-          temporibus voluptate enim rerum, pariatur reiciendis maxime est, sed
-          delectus iure quo perferendis repudiandae expedita aliquid ipsa
-          quaerat non perspiciatis illum, totam in saepe numquam? Minus, iure.
-          Blanditiis magnam, vitae cupiditate debitis porro in sequi totam
-          laboriosam placeat vero nobis repudiandae odit iusto, pariatur enim
-          praesentium possimus sed beatae dicta! Dicta illo maiores ipsum
-          tenetur expedita esse facilis optio aliquid quia voluptates. Nihil ad
-          nulla, mollitia accusamus fugit porro, nobis, quisquam quibusdam ullam
-          ratione beatae! Vero porro consequatur corrupti neque necessitatibus
-          beatae, ut fuga! Et quos ratione unde molestiae ab minima corporis non
-          sit eius ex eaque beatae, quod aperiam maxime itaque aliquid, quasi
-          placeat! Fugiat amet consequatur qui voluptatibus fuga quis nam
-          deserunt ut explicabo odio doloremque saepe nemo esse dolorum minima,
-          tempora praesentium quam consectetur excepturi et repudiandae vitae
-          quisquam tenetur. Aut quos animi placeat doloremque nihil? Aliquam
-          rerum id iusto corrupti. Doloribus repellendus expedita neque ea optio
-          molestiae, voluptate autem quidem! Debitis impedit amet reprehenderit
-          libero, minus fugit. Id iusto, maiores hic molestiae labore sed at
-          assumenda harum. Delectus veritatis saepe libero illo tempore at
-          explicabo repudiandae, rem odio cum beatae fuga illum doloribus animi
-          ipsam iure laudantium, amet, mollitia voluptates! Sapiente deleniti
-          laboriosam hic ipsa assumenda, enim tenetur veritatis ratione veniam
-          eligendi libero, incidunt, soluta asperiores unde? Perferendis
-          veritatis quasi voluptate et atque assumenda! Delectus architecto
-          sapiente deleniti fugit dolores animi, pariatur sunt odit dolorum et
-          dolor doloribus soluta quas, velit fugiat libero, in ex totam quasi
-          exercitationem quidem nesciunt explicabo. Eos perspiciatis numquam
-          fuga reiciendis dolore non minima fugiat ipsam iste eaque aliquam,
-          autem doloribus harum consequuntur dolores earum illo pariatur nemo
-          corporis ea similique consequatur repellat consectetur. Veritatis nisi
-          laboriosam cumque accusamus excepturi optio iusto, ipsum deleniti
-          quaerat nesciunt esse, quo earum itaque, ullam quae.
-        </div>
-      </div>
+              {isEmptyCanvas ? (
+                <div className="canvas-placeholder">Draw Signature</div>
+              ) : null}
+            </div>
+          </div>
 
-      <div className="row pt-1">
-        <div className="col-md-12">
-          <PageSubTitle title="(Demo Customer Name)" />
+          <div className="col-md-12 pt-1 mb-5">
+            <PageSubTitle title="(Demo Customer Name)" />
 
-          <p className="fs-16 fw-400 mb-0">
-            Prepared By. <span className="fw-600">Arbi Mauday</span>, On 08
-            January 2024
-          </p>
+            <p className="fs-16 fw-400 mb-0">
+              Prepared By. <span className="fw-600">Arbi Mauday</span>, On 08
+              January 2024
+            </p>
+          </div>
+
+          <div className="col-md-4 offset-4 d-grid mt-3">
+            <button type="submit" className="btn btn-primary fs-16 fw-500 py-3">
+              Apply
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
